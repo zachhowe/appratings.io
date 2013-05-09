6864ae3ae172bc9e1eae59d463ddd0c428413831
Zach Howe <zach@zachowe.com>
2013-05-09 14:27:17 -0700

    Removed database configuration from source

--- bin/update.rb
+++ bin/update.rb
@@ -7,6 +7,6 @@ require_relative '../app/helpers/app_helper.rb'
 require_relative '../app/helpers/rating_helper.rb'
 require_relative '../app/helpers/update_helper.rb'
 
-ENV['MONGODB_URI'] = 'mongodb://Bx1whnVv0d4Hu3H:192T3X5T9172h8I@alex.mongohq.com:10023/app15505665'
+# ENV['MONGODB_URI'] = 'mongodb://Bx1whnVv0d4Hu3H:192T3X5T9172h8I@alex.mongohq.com:10023/app15505665'
 
 UpdateHelper.update
