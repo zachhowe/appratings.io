7acdd1e4d91995d96b773b6a52cb66d9abef31e5
Zach Howe <zach@zachhowe.com>
2015-06-14 22:36:43 -0700

    Add record count per app to app list response

--- app/helpers/app_helper.rb
+++ app/helpers/app_helper.rb
@@ -19,18 +19,29 @@ module AppRatings
         docs.each do |doc|
           app_id = doc['app_id']
           info = doc['info']
+          record_count = collection.count({'app_id' => app_id}) # self.number_of_records_for_app(app_id)
 
           unless info.nil?
             app_name = info['trackName']
 
             unless app_name.nil?
-              app = {:app_id => app_id, :app_name => app_name}
+              app = {:app_id => app_id, :app_name => app_name, :number_of_records => record_count}
 
               yield app
             end
           end
         end
       end
+    end
+
+    def self.number_of_records_for_app(app_id)
+      number_of_records = 0
+
+      DataHelper.open('apps') do |collection|
+        number_of_records = collection.count({'app_id' => app_id})
+      end
+
+      number_of_records
     end
 
     def self.find_app(app_id)
